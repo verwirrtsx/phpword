9adccc8912a58bd64dce69cfe3640225c0c7c1d2
verwirrtsx <876586671@qq.com>
2019-08-07 16:21:53 +0800

    the path change

--- controller/Add.php
+++ controller/Add.php
@@ -12,16 +12,25 @@ use think\Controller;
 use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\Common\Font;
 use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\TemplateProcessor;
+use  think\facade\Env;
 
 class Add extends Controller
 {
     public  function  index(){
+
+        $fliename =time().'.docx';
+        $save_path =Env::get('root_path')."public/doc/";
+        $url = "http://localhost/word/public/doc/";
         $tmp=new \PhpOffice\PhpWord\TemplateProcessor('http://localhost/word/public/word/tmp.docx');//打开模板
         //dump($tmp);
         $tmp->setValue('name','李四');//替换变量name
         $tmp->setValue('mobile','18888888888');//替换变量mobile
         $tmp->setValue('school','社会大学');//替换变量mobile
-        $tmp->saveAs('jianli.docx');//另存为
+        dump($url.$fliename);
+         $tmp->saveAs($save_path.$fliename);//另存为
+        //$tmp->saveAs('jianli.docx');//另存为
        // $tmp->save('php://output');
+        header("Location: $url$fliename");
     }
 }
